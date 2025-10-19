da2c2dec67e4a05c1ca0f27ceb4a520e392411ab
Aikhan Jumashukurov <ajumashukurov@gmail.com>
2025-10-19 02:52:06 -0400

    Fix Vercel conflict: remove compiled api/index artifacts

--- api/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-export default function vercelHandler(req: VercelRequest, res: VercelResponse): Promise<Object>;
-//# sourceMappingURL=index.d.ts.map
